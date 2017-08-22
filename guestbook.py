41908978edee2aebbc25ca2a3c754c5f12d96fb2
dexiao <blue_xiaolu@163.com>
2017-08-21 23:46:14 -0700

    second commit

--- guestbook.py
+++ guestbook.py
@@ -1,6 +1,6 @@
 #coding:utf8
 import shelve
-
+from datetime import datetime
 from flask import Flask,request,render_template,redirect,escape,Markup
 
 application = Flask(__name__)
@@ -33,7 +33,34 @@ def index():
     """首页
     使用模板显示页面
     """
-    return render_template('index.html')
+    greeting_list = load_data()
+    return render_template('index.html',greeting_list=greeting_list)
+
+@application.route('/post',methods={'POST'})
+def post():
+    """ 用于提交评论的URL
+    """
+    # 获取已提交的数据
+    name = request.form.get('name') # 名字
+    comment = request.form.get('comment') # 留言
+    create_at = datetime.now() # 投稿时间（当前时间）
+    # 保存数据
+    save_data(name, comment, create_at)
+    # 保存后重定向到首页
+    return redirect('/')
+
+@application.template_filter('n12br')
+def n12br_filter(s):
+    """将带有换行符号换成br标签
+    """
+    return escape(s).replace('\n',Markup('<br>'))
+
+@application.template_filter('datetime_fmt')
+def datetime_fmt_filter(dt):
+    """使用datetime对象更容易的分辨模块过滤器
+    """
+    return dt.strftime('%Y/%m/%d %H:%M:%S')
+
 
 if __name__ == '__main__':
     application.run('0.0.0.0',8000,debug=True)
